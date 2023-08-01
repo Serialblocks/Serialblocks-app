1614e54c91876afd7f5711c08692a8e764a03c92
kontb2a22 <ahmedxd.2009@gmail.com>
2023-08-01 20:47:21 +0300

    feat: add sensor required pill, refactor: terminal

--- client/src/components/Plant.jsx
+++ client/src/components/Plant.jsx
@@ -2,7 +2,7 @@ import { Card, CardContent } from "@/components/ui/card";
 
 const Plant = () => {
   return (
-    <Card className="row-span-2 col-span-6">
+    <Card className="row-span-2 col-span-6 bg-[#bc680e]">
       <CardContent className="row-span-2 col-span-2">
         <p>Plant</p>
       </CardContent>
