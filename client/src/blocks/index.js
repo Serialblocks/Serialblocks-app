c9a66892b3353d21fd3148bee9ecae719a78a094
kontb2a22 <ahmedxd.2009@gmail.com>
2023-11-11 22:55:21 +0200

    chore: move files from /client to root directory

--- client/src/blocks/index.js
+++
@@ -1,9 +0,0 @@
-import { Terminal } from "@/blocks/Terminal";
-import { LineChart } from "@/blocks/LineChart";
-import { Processor } from "@/blocks/Processor";
-import { SerialPortForm } from "@/blocks/SerialPortForm";
-import { RGB } from "@/blocks/RGB";
-import { LED } from "@/blocks/LED";
-import { Humidity } from "@/blocks/Humidity";
-
-export { Terminal, LineChart, Processor, RGB, LED, Humidity, SerialPortForm };
--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import { Notification } from "@/components/Notification";
-import { Footer } from "@/components/Footer";
-import { ConfigForm } from "@/components/ConfigForm";
-
-export { Notification, Footer, ConfigForm };
